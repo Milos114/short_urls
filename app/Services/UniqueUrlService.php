5def2665e23c88f83e554d445c22de2a4f374a66
milos <mstefanovic40@gmail.com>
2024-03-25 10:20:40 +0100

    UniqueUrlService.php refactor

--- app/Services/UniqueUrlService.php
+++ app/Services/UniqueUrlService.php
@@ -9,14 +9,9 @@ class UniqueUrlService
 {
     public function handle(): string
     {
-        $stringAppends = Url::pluck('short_url')->map(function ($url) {
-            $array = explode('/', $url);
-            return end($array);
-        })->all();
-
         $hash = Str::random(6);
 
-        if (in_array($hash, $stringAppends, true)) {
+        if (in_array($hash, Url::pluck('hash')->all(), true)) {
             $this->handle();
         }
 
